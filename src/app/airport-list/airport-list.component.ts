283f2debe055862405e5a294f39444c03da12c66
Markus Eckstein <markus@markuseckstein.de>
2018-08-29 21:39:48 +0200

    chore: update deps

--- src/app/airport-list/airport-list.component.ts
+++ src/app/airport-list/airport-list.component.ts
@@ -1,22 +1,17 @@
-import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
-import { Airport } from '../shared/airport';
+import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+import { Airport } from "../shared/airport";
 
 @Component({
-  selector: 'app-airport-list',
-  templateUrl: './airport-list.component.html',
-  styleUrls: ['./airport-list.component.css'],
+  selector: "app-airport-list",
+  templateUrl: "./airport-list.component.html",
+  styleUrls: ["./airport-list.component.css"],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AirportListComponent implements OnInit {
-  @Input() airports: Airport[];
+export class AirportListComponent {
+  @Input()
+  airports: Airport[];
 
   trackByCode(airport: Airport) {
     return airport.code;
   }
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
 }
--- src/app/location.service.ts
+++ src/app/location.service.ts
@@ -1,12 +1,10 @@
-import { MapsAPILoader } from '@agm/core';
-import { Injectable } from '@angular/core';
-
-import { Observable } from 'rxjs/Observable';
-import { airportCodes } from './airports';
-import { Airport } from './shared/airport';
-import { ReplaySubject } from 'rxjs/ReplaySubject';
-import { Subject } from 'rxjs/Subject';
-import { StorageService } from './storage/storage.service';
+import { MapsAPILoader } from "@agm/core";
+import { Injectable } from "@angular/core";
+import { concat, from, Observable, ReplaySubject, Subject, timer } from "rxjs";
+import { delayWhen, flatMap, tap, map, retryWhen } from "rxjs/operators";
+import { airportCodes } from "./airports";
+import { Airport } from "./shared/airport";
+import { StorageService } from "./storage/storage.service";
 
 declare var google: any;
 
@@ -18,16 +16,21 @@ export class LocationService {
 
   private airportArray = new Array<Airport>();
 
-  constructor(private _mapsApiLoader: MapsAPILoader, private storage: StorageService) {
-    _mapsApiLoader.load()
+  constructor(
+    private _mapsApiLoader: MapsAPILoader,
+    private storage: StorageService
+  ) {
+    _mapsApiLoader
+      .load()
       .then(() => {
-        Observable.timer(200).subscribe(() => {
+        timer(200).subscribe(() => {
           this.geocoder = new google.maps.Geocoder();
-          console.log('Maps api initialized:', this.geocoder);
+          console.log("Maps api initialized:", this.geocoder);
           this.loadAirports();
         });
-      }).catch(err => {
-        console.log('Error during maps api initialization: ', err);
+      })
+      .catch(err => {
+        console.log("Error during maps api initialization: ", err);
       });
   }
 
@@ -55,52 +58,54 @@ export class LocationService {
       }
     });
 
-    Observable.concat(
-      Observable.from(airportsFromCache),
-      Observable.from(copyOfAirportCodes)
-        .do(() => {
+    concat(
+      from(airportsFromCache),
+      from(copyOfAirportCodes).pipe(
+        tap(() => {
           if (idx % 6 === 0) {
             counter++;
           }
           idx++;
-        })
-        .delayWhen((code: any) => {
+        }),
+        delayWhen((code: any) => {
           const waitTime = 3050 * counter;
           console.log(`waitTime for ${code}, idx ${counter} is ${waitTime}`);
-          return Observable.timer(waitTime);
-        })
-        .flatMap(x => this.codeAirport(x))
-    )
-    .subscribe(airport => {
+          return timer(waitTime);
+        }),
+        flatMap(x => this.codeAirport(x))
+      )
+    ).subscribe(airport => {
       this.airportArray.push(airport);
       this.airports.next(this.airportArray.slice());
     });
   }
 
-
   private codeAirport(airportCode: string): Observable<Airport> {
     return Observable.create(obs => {
       console.log(`loading airport '${airportCode}'`);
-      this.geocoder.geocode({
-        'address': airportCode + ' Airport'
-      }, (response, status) => {
-        if (status === 'OK') {
-          obs.next(response);
-          obs.complete();
-        } else {
-          console.log('airport: ' + airportCode, status);
-          obs.error({ error: status });
+      this.geocoder.geocode(
+        {
+          address: airportCode + " Airport"
+        },
+        (response, status) => {
+          if (status === "OK") {
+            obs.next(response);
+            obs.complete();
+          } else {
+            console.log("airport: " + airportCode, status);
+            obs.error({ error: status });
+          }
         }
-      });
-    })
-      .map((results) => {
+      );
+    }).pipe(
+      map((results: any) => {
         if (results.error) {
-          console.warn('Geo error:', results);
+          console.warn("Geo error:", results);
           return {
             code: airportCode,
             allData: results[0],
-            label: 'Error ' + results[1],
-            title: airportCode,
+            label: "Error " + results[1],
+            title: airportCode
           };
         } else {
           console.log(`Success ${airportCode}`, results);
@@ -110,14 +115,18 @@ export class LocationService {
             name: results[0].address_components[0].long_name,
             label: results[0].formatted_address,
             lat: results[0].geometry.location.lat(),
-            lng: results[0].geometry.location.lng(),
+            lng: results[0].geometry.location.lng()
           };
           return marker;
         }
-      })
-      .do(apt => this.storage.setAirport(apt))
-      .retryWhen(errors => errors
-        .do(val => console.log(`Error for ${airportCode}`, val))
-        .delayWhen(val => Observable.timer(Math.random() * 4000)));
+      }),
+      tap((apt: any) => this.storage.setAirport(apt)),
+      retryWhen(errors =>
+        errors.pipe(
+          tap(val => console.log(`Error for ${airportCode}`, val)),
+          delayWhen(val => timer(Math.random() * 4000))
+        )
+      )
+    );
   }
 }
